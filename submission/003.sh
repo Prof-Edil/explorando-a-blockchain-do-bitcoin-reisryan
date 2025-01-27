d04510e412cdfe1da760a4019cc390ffae756014
reisryan <91282891+reisryan@users.noreply.github.com>
2025-01-27 15:59:31 -0300

    Update 003.sh

--- submission/003.sh
+++ submission/003.sh
@@ -1,1 +1,6 @@
 # How many new outputs were created by block 123,456?
+BLOCK_HEIGHT=123456
+BLOCK_HASH=$(bitcoin-cli getblockhash $BLOCK_HEIGHT)
+BLOCK_DATA=$(bitcoin-cli getblock $BLOCK_HASH 2)
+NEW_OUTPUTS=$(echo "$BLOCK_DATA" | jq '[.tx[].vout | length] | add')
+echo "$NEW_OUTPUTS"
