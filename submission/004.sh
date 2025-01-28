fd9ca5c10bfa67d19af60c7e0b0d21713b0cb1ea
reisryan <91282891+reisryan@users.noreply.github.com>
2025-01-28 17:07:17 -0300

    Update 004.sh

--- submission/004.sh
+++ submission/004.sh
@@ -1,8 +1,8 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
 XPUB="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
-DESCRIPTOR="tr([$XPUB/0'/0']99)"
+DESCRIPTOR="tr([$XPUB/0/0]99)"
 DESCRIPTOR_INFO=$(bitcoin-cli getdescriptorinfo "$DESCRIPTOR")
 DESCRIPTOR_WITH_CHECKSUM=$(echo "$DESCRIPTOR_INFO" | jq -r '.descriptor')
-# ADDRESS=$(bitcoin-cli deriveaddresses "$DESCRIPTOR_WITH_CHECKSUM")
-echo "$DESCRIPTOR_WITH_CHECKSUM"
+ADDRESS=$(bitcoin-cli deriveaddresses "$DESCRIPTOR_WITH_CHECKSUM")
+echo "$ADDRESS"
