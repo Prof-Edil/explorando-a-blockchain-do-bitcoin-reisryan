94aa51abe535fbc8ce1a26f1c10ca0ea9f391228
reisryan <91282891+reisryan@users.noreply.github.com>
2025-01-29 13:47:38 -0300

    Update 005.sh

--- submission/005.sh
+++ submission/005.sh
@@ -2,9 +2,8 @@
 #   `37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517`
 TXID="37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517"
 RAW_TX=$(bitcoin-cli getrawtransaction "$TXID" true)
-PUBKEYS=$(echo "$RAW_TX" | jq -r '.vin[].scriptSig.asm' | grep -Eo "[0-9a-f]{66,130}")
-PUBKEY_ARRAY=($(echo "$PUBKEYS" | head -n 4))
-PUBKEY_JSON=$(printf '"%s",' "${PUBKEY_ARRAY[@]}")
+PUBKEYS=($(echo "$RAW_TX" | jq -r '.vin[].txinwitness[]' | grep -E "^[0-9a-f]{66,130}$"))
+PUBKEY_JSON=$(printf '"%s",' "${PUBKEYS[@]}")
 PUBKEY_JSON="[${PUBKEY_JSON%,}]"
 MULTISIG=$(bitcoin-cli createmultisig 1 "$PUBKEY_JSON")
 P2SH_ADDRESS=$(echo "$MULTISIG" | jq -r '.address')
