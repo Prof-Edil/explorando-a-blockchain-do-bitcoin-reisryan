e48d09ccbdc1b8757185e666b1ad12353c453e43
reisryan <91282891+reisryan@users.noreply.github.com>
2025-01-29 17:18:37 -0300

    Update 007.sh

--- submission/007.sh
+++ submission/007.sh
@@ -5,14 +5,12 @@ BDATA="$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_259 -rpcpassw
 echo "$BDATA" | jq -r '.tx[].txid' | while read -r TX; do
   TX_OUTS=$(bitcoin-cli getrawtransaction "$TX" true)
 
-  # Verifique se TX_OUTS tem uma resposta válida
   if [ -n "$TX_OUTS" ]; then
     echo "$TX_OUTS" | jq -c '.vout[] | select(.scriptPubKey.addresses != null)' | while read -r TXOUTN; do
       OUT_INDEX=$(echo "$TXOUTN" | jq -r '.n')
       OUT_ADDRESS=$(echo "$TXOUTN" | jq -r '.scriptPubKey.addresses[0]')
-      OUT_STATUS=$(bitcoin-cli gettxout "$TX" "$OUT_INDEX")
+      OUT_STATUS=$(bitcoin-cli gettxout "$OUT_ADDRESS" 1)
 
-      # Verifica se o OUT_STATUS não está vazio e é um output não gasto
       if [ -n "$OUT_STATUS" ]; then
         echo "$OUT_ADDRESS"
       fi
