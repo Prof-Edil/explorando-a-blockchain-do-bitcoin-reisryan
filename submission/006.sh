581eb0f381e29bdbd54dd4e53e81303dcaffcef7
reisryan <91282891+reisryan@users.noreply.github.com>
2025-01-29 13:58:35 -0300

    Update 006.sh

--- submission/006.sh
+++ submission/006.sh
@@ -1,1 +1,3 @@
 # Which tx in block 257,343 spends the coinbase output of block 256,128?
+GET_BLOCK="$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_259 -rpcpassword=CsaSE6fmbyP0 getblock 256128 2)"
+echo "$GET_BLOCK"
