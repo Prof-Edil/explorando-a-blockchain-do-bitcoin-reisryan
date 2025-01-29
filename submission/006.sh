059611bab6081d61bd82ac640c8b1d4c7bec79ac
reisryan <91282891+reisryan@users.noreply.github.com>
2025-01-29 14:40:35 -0300

    Update 006.sh

--- submission/006.sh
+++ submission/006.sh
@@ -2,7 +2,7 @@
 GET_HASH1="$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_259 -rpcpassword=CsaSE6fmbyP0 getblockhash 256128)"
 GET_BLOCK="$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_259 -rpcpassword=CsaSE6fmbyP0 getblock "$GET_HASH1" 2)"
 TXID=$(echo "$GET_BLOCK" | jq -r '.tx[0].txid')
-GET_HASH2="$(bitcoin-cli -rpconnect=84.247.182.145:8332 -rpcuser=user_259 -rpcpassword=CsaSE6fmbyP0 getblockhash 257343)"
+GET_HASH2="$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_259 -rpcpassword=CsaSE6fmbyP0 getblockhash 257343)"
 TXS=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_259 -rpcpassword=CsaSE6fmbyP0 getblock "$GET_HASH2" 2)
 INPUT_TX=$(echo "$TXS" | jq -r --arg TXID "$TXID" '.tx[] | select(.vin[].txid == $TXID) | .txid')
 echo "$INPUT_TX"
