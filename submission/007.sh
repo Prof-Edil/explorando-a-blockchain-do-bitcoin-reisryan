47121b8f7f6e7e0232923ae20cf807da4e4da616
reisryan <91282891+reisryan@users.noreply.github.com>
2025-01-29 17:34:14 -0300

    Update 007.sh

--- submission/007.sh
+++ submission/007.sh
@@ -5,17 +5,13 @@ BDATA="$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_259 -rpcpassw
 echo "$BDATA" | jq -r '.tx[].txid' | while read -r TX; do
   TX_OUTS=$(bitcoin-cli getrawtransaction "$TX" true)
 
-  if [ -n "$TX_OUTS" ]; then
-    echo "$TX_OUTS" | jq -c '.vout[] | select(.scriptPubKey.addresses != null)' | while read -r TXOUTN; do
-      OUT_INDEX=$(echo "$TXOUTN" | jq -r '.n')
-      OUT_ADDRESS=$(echo "$TXOUTN" | jq -r '.scriptPubKey.addresses[0]')
-      OUT_STATUS=$(bitcoin-cli gettxout "$OUT_ADDRESS" 1)
+  echo "$TX_OUTS" | jq -c '.vout[] | select(.scriptPubKey.addresses != null)' | while read -r TXOUTN; do
+    OUT_INDEX=$(echo "$TXOUTN" | jq -r '.n')
+    OUT_ADDRESS=$(echo "$TXOUTN" | jq -r '.scriptPubKey.addresses[0]')
+    OUT_STATUS=$(bitcoin-cli gettxout "$TX" "$OUT_INDEX")
 
-      if [ -n "$OUT_ADDRESS" ]; then
-        echo "$OUT_ADDRESS"
-      fi
-    done
-  else
-    echo "Erro: resposta vazia ou mal formatada para getrawtransaction do TX $TX" >&2
-  fi
+    if [ -n "$OUT_STATUS" ]; then
+      echo "Endereço encontrado: $OUT_ADDRESS"
+    fi
+  done
 done
