87260ffa50c369e22b442520dea2b1d0b4d9261a
reisryan <91282891+reisryan@users.noreply.github.com>
2025-01-29 21:59:17 -0300

    Update 007.sh

--- submission/007.sh
+++ submission/007.sh
@@ -1,19 +1,15 @@
 # Only one single output remains unspent from block 123,321. What address was it sent to?
-#!/bin/bash
-
 BHASH="$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_259 -rpcpassword=CsaSE6fmbyP0 getblockhash 123321)"
 BDATA="$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_259 -rpcpassword=CsaSE6fmbyP0 getblock "$BHASH" 2)"
 
 if [ -z "$BDATA" ]; then
-  echo "Erro: Não foi possível obter os dados do bloco."
   exit 1
 fi
 
 echo "$BDATA" | jq -r '.tx[].txid' | while read -r TX; do
   TX_OUTS=$(bitcoin-cli getrawtransaction "$TX" true)
-  
-  if [ -z "$TX_OUTS" ]; then
-    echo "Erro: Não foi possível obter os dados da transação $TX."
+
+  if [ -z "$TX_OUTS" ] || [ "$TX_OUTS" == "null" ]; then
     continue
   fi
 
@@ -21,10 +17,14 @@ echo "$BDATA" | jq -r '.tx[].txid' | while read -r TX; do
     OUT_INDEX=$(echo "$TXOUTN" | jq -r '.n')
     OUT_ADDRESS=$(echo "$TXOUTN" | jq -r '.scriptPubKey.addresses[0]')
     
+    if [ -z "$OUT_ADDRESS" ] || [ "$OUT_ADDRESS" == "null" ]; then
+      continue
+    fi
+
     OUT_STATUS=$(bitcoin-cli gettxout "$TX" "$OUT_INDEX")
 
     if [ -n "$OUT_STATUS" ]; then
-      echo "Endereço encontrado: $OUT_ADDRESS"
+      echo "$OUT_ADDRESS"
     fi
   done
 done
