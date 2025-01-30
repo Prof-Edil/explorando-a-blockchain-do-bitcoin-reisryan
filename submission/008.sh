968bf6f5d0f9ff25356dc27c06f2dd1a3c163cf7
reisryan <91282891+reisryan@users.noreply.github.com>
2025-01-29 23:57:37 -0300

    Update 008.sh

--- submission/008.sh
+++ submission/008.sh
@@ -1,7 +1,6 @@
 # Which public key signed input 0 in this tx:
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
 TXID="e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163"
-PREV_TXID=$(bitcoin-cli getrawtransaction "$TXID" true | jq -r '.vin[0].txid')
-PUBKEY=$(bitcoin-cli getrawtransaction "$PREV_TXID" true | jq -r '.vout[0].scriptPubKey.hex')
+SCRIPT=$(bitcoin-cli -rpcconnect=84.247.182.145:8332 -rpcuser=user_259 -rpcpassword=CsaSE6fmbyP0 getrawtransaction "$TXID" true | jq -r '.vin[0].txinwitness[2]')
+PUBKEY=$(echo "$SCRIPT" | cut -c7-72)
 echo "$PUBKEY"
-
